7980719b38d2a312b8ed3a18aa8a159fa5529ad3
Madhusoodhanan A <a.madhusoodhanan1996@gmail.com>
2022-04-10 20:04:40 +0530

    Add the command-line arguments for the wtr application

--- wtr/main.py
+++ wtr/main.py
@@ -1,10 +1,23 @@
 """`wtr` main entry point."""
+import argparse
 
 
 def main():
     """Run the `wtr` app."""
-    print("Hello, world!")
+    parser = argparse.ArgumentParser(
+        description="wtr is a command-line weather application. It shows the weather for the given locations."
+    )
+    initialize_parser(parser)
+    parser.parse_args()
 
 
-if __name__ == '__main__':
+def initialize_parser(parser):
+    """Initialize the required arguments for the `wtr` app."""
+    parser.add_argument("location", help="Location to get weather for.", nargs="+")
+    parser.add_argument(
+        "-w", "--week", help="Show the weather for the next week starting from today.", action="store_true"
+    )
+
+
+if __name__ == "__main__":
     main()
